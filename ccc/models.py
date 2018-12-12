34b28eec0fa3ad63cf6fdb01438bce15eb1072ba
Ben <bsmiller25@gmail.com>
2018-12-12 18:54:57 -0500

    add a member model

--- ccc/models.py
+++ ccc/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+
+
+class Member(models.Model):
+    first_name = models.CharField(max_length=100,
+                                  null=True,
+                                  blank=True)
+    last_name = models.CharField(max_length=100,
+                                 null=True,
+                                 blank=True)
+    uscf_id = models.BigIntegerField(null=True,
+                                     blank=True)
+    chesscom_userid = models.CharField(null=True,
+                                       blank=True)
+    lichess_userid = models.CharField(null=True,
+                                      blank=True)
