f85b01a4c4e487b7fafa97d2ec9faf436551e3e5
Ben <bsmiller25@gmail.com>
2018-12-21 14:57:06 -0500

    adding content

--- ccc/views.py
+++ ccc/views.py
@@ -1,7 +1,15 @@
+import os
 from django.http import HttpResponse
 from django.shortcuts import render
 
 
 
 def index(request):
-    return render(request, 'ccc/index.html')
+
+    mapbox_key = os.environ.get('MAPBOX_KEY')
+    
+    context = {
+        'mapbox_key': mapbox_key
+        }
+    
+    return render(request, 'ccc/index.html', context)
