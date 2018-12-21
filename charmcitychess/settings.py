1ff569ad0ef257f382a2695d3634fe92b9a1d29d
Ben <bsmiller25@gmail.com>
2018-12-21 18:30:50 -0500

    update settings

--- charmcitychess/settings.py
+++ charmcitychess/settings.py
@@ -23,9 +23,10 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get('SECRET_KEY')
+DEBUG = os.environ.get('DEBUG')
 
 ALLOWED_HOSTS = [
+    'charmcitychess.com',
     'charmcitychess.herokuapp.com',
     '127.0.0.1',
 ]
