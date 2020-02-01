e18a5c200cd1a05c558134d9d995f60e78072f75
Ben Miller <bsmiller25@gmail.com>
2020-02-01 13:54:42 -0500

    toggle sandbox mode for sendgrid

--- charmcitychess/settings.py
+++ charmcitychess/settings.py
@@ -141,7 +141,7 @@ EMAIL_BACKEND = "sendgrid_backend.SendgridBackend"
 SENDGRID_API_KEY = os.environ.get("SENDGRID_API")
 
 # Toggle sandbox mode (when running in DEBUG mode)
-SENDGRID_SANDBOX_MODE_IN_DEBUG=False
+SENDGRID_SANDBOX_MODE_IN_DEBUG=True
 
 # echo to stdout or any other file-like object that is passed to the backend via the stream kwarg.
 SENDGRID_ECHO_TO_STDOUT=True
