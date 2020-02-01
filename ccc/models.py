3bc1623b46300bc796502f8144aa1d0b3701d2f2
Ben Miller <bsmiller25@gmail.com>
2020-02-01 10:48:41 -0500

    add member model and link to user

--- ccc/models.py
+++ ccc/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
-# Create your models here.
+
+class Member(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    uscf_id = models.IntegerField(null=True, blank=True)
+    chesscom_id = models.CharField(max_length=100, null=True, blank=True)
+    lichess_id = models.CharField(max_length=100, null=True, blank=True)
+
+    def __str__(self):
+        return('{}, {}'.format(self.user.first_name, self.user.last_name))
+    
+@receiver(post_save, sender=User)
+def create_user_member(sender, instance, created, **kwargs):
+    if created:
+        Member.objects.create(user=instance)
+
+@receiver(post_save, sender=User)
+def save_user_member(sender, instance, **kwargs):
+    instance.member.save()
